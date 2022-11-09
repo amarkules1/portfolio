23cdc7c0626d43cfdc7dfe150b344e93096cf48d
Alex Markules <amarlar2354@gmail.com>
2022-11-08 21:12:50 -0500

    followed a template, ready to customize

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Navigation,
+  Footer,
+  Home,
+  About,
+  Contact,
+  Blog,
+  Posts,
+  Post,
+} from "./components";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/blog" element={<Blog />}>
+        <Route path="" element={<Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route>
+    </Routes>
+    <Footer />
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
