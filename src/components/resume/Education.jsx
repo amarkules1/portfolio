a2ace996a0ef4e5668eb1364a491ae7f239abcdc
Alex Markules <amarlar2354@gmail.com>
2023-03-11 12:19:58 -0500

    fix education page

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -30,16 +30,10 @@ class Education extends Component {
                 <div className="company">Western Michigan University</div>
                 <div className="location">Kalamazoo, Michigan</div>
                 <ul className="details">
-                    <li className="detail">Developed software tools to optimize sales revenue, updating hundreds of thousands of prices weekly
-                     and bringing in $10 million in additional revenue to the company in the first year.</li>
-                    <div className="detail">Designed and implemented web applications to provide sales and revenue management teams with better
-                     control of pricing for individual customers, saving these teams hundreds of hours of work annually.</div>
-                    <li className="detail">Participated in design and planning of integrations between custom-built applications and a new SAP 
-                    based ERP system, integrating via REST and OData services and seizing opportunities to improve from legacy systems.</li>
-                    <li className="detail">Served in an on-call rotation to assess and correct production issues, minimizing downtime.</li>
-                    <li className="detail">Trained and assisted new developers on the team, to help them understand the business use 
-                    of our applications, as well as some of our software development best practices like Test Driven Development and 
-                    Automated Acceptance Testing.</li>
+                    <li className="detail">Learned to effectively use and implement a wide variety of computer science concepts, 
+                    ranging from basic data structures to stochastic computing algorithms.</li>
+                    <li className="detail">Used industry best practices in agile programming and software testing 
+                    to develop a final project which had real world use for guidance counselors at the university.</li>
                 </ul>
             </div>
         </div>
