413ac998dbe337bba290e79c1c40a97cc0b84d26
Alex Markules <amarlar2354@gmail.com>
2023-11-24 10:01:01 -0500

    make navbar responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,16 +2,38 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  isMobile(){
+    return window.innerWidth < 768;
+  }
+
+  toggleMenu(){
+    this.setState({ showMenu: !this.state.showMenu })
+  }
+
   render() {
+    const showMenu = this.state.showMenu;
+    const isMobile = this.isMobile();
     return (
       <div className="navigation">
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <div className="container">
+          <div className={"container" + (this.isMobile() ? " navbarContainerMobile" : "")}>
             <NavLink className="navbar-brand" to="/">
               Alex Markules
             </NavLink>
-            <div>
-              <ul className="navbar-nav ml-auto">
+            <button className={"navbar-toggler" + (this.isMobile() ? " displayToggle" : "")} type="button" onClick={this.toggleMenu}>
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            { (showMenu || !isMobile) && (
+            <div className="collapse navbar-collapse" >
+              <ul className={"navbar-nav ml-auto" + (this.isMobile() ? " mobileMenuUl" : "")}>
                 <li className="nav-item">
                   <NavLink className="nav-link" to="/">
                     Home
@@ -34,6 +56,7 @@ class Navigation extends Component {
                 </li>
               </ul>
             </div>
+            )}
           </div>
         </nav>
       </div>
